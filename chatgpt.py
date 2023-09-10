18b2c278dacfef7ca3855724c2024ab3f633704d
Brandon Vo <brtroungvo@gmail.com>
2023-09-09 17:08:37 -0700

    Add Develop branch

--- chatgpt.py
+++ chatgpt.py
@@ -2,8 +2,8 @@ import os
 import openai
 from dotenv import load_dotenv
 
-# Overrides system variables with .env variables
+# Loads and overrides system variables with .env variables
 load_dotenv(override=True)
 
-# Load API key from environment variable
+# Get API key from environment variable
 openai.api_key = os.getenv("CHATGPT_API_KEY")
