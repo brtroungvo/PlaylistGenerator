f8efe86a443769f6c9ef505997c1c2b4c2b63517
Javi Wu <javiwu892@gmail.com>
2023-09-10 00:55:12 -0700

    edited YouTubeSearch.py

--- YouTubeSearch.py
+++ YouTubeSearch.py
@@ -1,24 +1,31 @@
 # importing
 import os
+from dotenv import load_dotenv
 from googleapiclient.discovery import build
+import chatgpt
+
+# Load YouTube apikey
+load_dotenv(override=True)
 
 # Creating YouTube Data api cleint
-
-youtube = build('youtube', 'v3', devlopeKey=api.key)
+developerKey = os.getenv("YOUTUBE_API_KEY")
+youtube = build('youtube', 'v3', developerKey)
 
 
 # Stores results for YouTubeSet Searches
-youtubeLinks = set()
+youtubeLinks = []
 
 # Search Request
-youtubeSet = {"song1", "song2", "song3"} #placeholder
-for x in youtubeSet:
-    search_question = x
-    search
 
+youtubeList = songList
+for x in youtubeList:
+    search_response = youtube.search().list(
+        q = x,
+        type = 'video',
+        maxResults = 10
+     ).execute()
+    print ("--------------------")
 
-
-search_reponse = youtube.search()
 
 # Adding Videos to Playlist
 
