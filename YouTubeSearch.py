c4f2394c351fcae25053a630e78e0a9caaac59e2
Brandon Vo <brtroungvo@gmail.com>
2023-09-10 19:12:15 -0700

    Edit YouTubeSearch.py

--- YouTubeSearch.py
+++ YouTubeSearch.py
@@ -1,15 +1,15 @@
 # importing
 import os
+import googleapiclient.discovery
 from dotenv import load_dotenv
-from googleapiclient.discovery import build
-import chatgpt
+#from chatgpt import songList
 
 # Load YouTube apikey
 load_dotenv(override=True)
 
 # Creating YouTube Data api cleint
-developerKey = os.getenv("YOUTUBE_API_KEY")
-youtube = build('youtube', 'v3', developerKey)
+apikey = os.getenv("YOUTUBE_API_KEY")
+youtube = googleapiclient.discovery.build('youtube', 'v3', developerKey=apikey)
 
 
 # Stores results for YouTubeSet Searches
@@ -17,15 +17,35 @@ youtubeLinks = []
 
 # Search Request
 
+'''
 youtubeList = songList
 for x in youtubeList:
-    search_response = youtube.search().list(
-        q = x,
-        type = 'video',
-        maxResults = 10
-     ).execute()
-    print ("--------------------")
+    try:
+        search_response = youtube.search().list(
+            q = x,
+            type = 'video',
+            part = 'snippet',
+            maxResults = 10
+        ).execute()
+        print ("\n----------------------------------------\n")
+    except Exception as e:
+        print (f"An error occured: {e}")
+'''
 
+search_query = "mariah carrey"
+
+try:
+    print ("\n----------------------------------------\n")
+    search_response = youtube.search().list(
+        q = search_query,
+        type = 'video',
+        part = 'snippet',
+        maxResults = 3
+    ).execute()
+    print (search_response)
+    print ("\n----------------------------------------\n")
+except Exception as e:
+    print (f"An error occured: {e}")
 
 # Adding Videos to Playlist
 
