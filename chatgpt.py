73f4037b32021fda119b98c139a6510e0d37c4c6
Brandon Vo <brtroungvo@gmail.com>
2024-10-14 14:50:59 -0700

    Updated code for new ChatGPT model 4o

--- chatgpt.py
+++ chatgpt.py
@@ -1,29 +1,27 @@
 import os
-import openai
+from openai import *
 from dotenv import load_dotenv
 
 # Loads and overrides system variables with .env variables
 load_dotenv(override=True)
 
-# Get API key from environment variable
-openai.api_key = os.getenv("CHATGPT_API_KEY")
-
 # Gets user input for genre of songs
 genre = input("What genre of music do you want in your playlist?\n")
 
 # Prompt that goes into chat gpt
-prompt = "List of 10" + genre + "songs with the artist name. No note at the end."
+prompt = "List of 10 " + genre + " songs with the artist name. No note at the end."
 
+client = OpenAI(api_key=os.getenv('CHATGPT_API_KEY'))
 
 # DOES NOT USE PREVIOUS MESSAGES AS CONTEXT (NO CONVERSATION)
-response = openai.ChatCompletion.create(
-    model = "gpt-3.5-turbo",
+response = client.chat.completions.create(
+    model = "gpt-4o",
     messages = [
         {"role": "user", "content": prompt}
-    ]
+    ],
 )
-assistantResponse = response['choices'][0]['message']['content']
-result = assistantResponse.strip(".")
+assistantResponse = response.choices[0].message.content
+result = assistantResponse
 print(f"ChatGPT:\n{result}")
 
 print("\n----------------------------------------\n")
